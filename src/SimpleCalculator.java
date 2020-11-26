59f7f65cda51e20230e9ab6cb78060ea7ba92531
Justin Rickert <rickerju@mail.gvsu.edu>
2020-11-25 23:41:49 -0500

    I moved over to eclipse, hopefully this works now

---
+++ src/SimpleCalculator.java
@@ -0,0 +1,19 @@
+package src;
+
+public class SimpleCalculator {
+    public int add(int a, int b) {
+        return a + b;
+    }
+    public int sub(int a, int b) {
+        return a - b;
+    }
+    public int mul(int a, int b) {
+        return a * b;
+    }
+    public int div(int a, int b) {
+        return a / b;
+    }
+    public static final void main(String[] args) {
+        System.out.println("Hey there CIS350.");
+    }
+}
